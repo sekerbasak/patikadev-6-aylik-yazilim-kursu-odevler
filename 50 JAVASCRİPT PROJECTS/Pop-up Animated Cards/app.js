ef5892e4154d43dd7ac64fb02fa82709a3ebba49
Başak Şeker <basak.skr.551@gmail.com>
2023-09-19 18:44:31 +0300

    The first of 50 projects has been uploaded

---
+++ 50 JAVASCRİPT PROJECTS/Pop-up Animated Cards/app.js
@@ -0,0 +1,13 @@
+const panels=document.querySelectorAll(".panel");
+panels.forEach((panel)=>{
+  panel.addEventListener("click",()=>{
+    removeActive();
+    panel.classList.add("active")
+  })
+})
+
+function removeActive(){
+    panels.forEach(panel => {
+        panel.classList.remove("active");
+    });
+}
